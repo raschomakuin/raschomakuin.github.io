c8c9436dd77d56955166beac719a0b749ada5a41
raschomakuin <rodripalacio14@gmail.com>
2026-02-22 15:33:15 -0300

    recarga de la web original

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,11 @@
-{ defineConfig } from 'astro/config'; import tailwind from '@astrojs/tailwind'; // https://astro.build/config export default defineConfig({ integrations: [tailwind()], output: 'static', compressHTML: true, build: { inlineStylesheets: 'auto' } });
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'static',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto'
+  }
+});
